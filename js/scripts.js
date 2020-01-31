de2cfe03d1c83686f0dc3f1dadf1c3bd3aae73f8
Student <example@example.com>
2020-01-31 09:59:02 -0800

    add values to toppings and sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,31 @@
 //back
-function Pizza () {
-  this.tops = [];
-
-}
-
-Pizza.prototype.addTops = function(toppings){
-  this.tops.push(toppings);
-}
-
-
-
-
-
-
-
-
-
-function Toppings (top1, top2, top3){
+function Pizza (size, top1, top2, top3) {
   this.size = size,
   this.top1 = top1,
   this.top2 = top2,
   this.top3 = top3
+
 }
 
 
 
 
-//front
+
+
+
+
+
+
+// //front
+
+
+
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var size = $("select#size").val();
-    var protein = $("select#top1").val();
-    var spice = $("select#top2").val();
-    var veggie = $("select#top3").val();
-    var newPizza = new Pizza (size, protein, spice, veggie);
-    Pizza.addTops(newPizza);
 
-
+    
 
 
   })
