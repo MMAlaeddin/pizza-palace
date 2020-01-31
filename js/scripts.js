0e4edff409dbcf57044c51880c2e8ada19201b61
Student <example@example.com>
2020-01-31 08:54:08 -0800

    add pizza and topping constructors

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,7 @@ Pizza.prototype.addTops = function(toppings){
 
 
 function Toppings (top1, top2, top3){
+  this.size = size,
   this.top1 = top1,
   this.top2 = top2,
   this.top3 = top3
@@ -30,6 +31,11 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var size = $("select#size").val();
+    var protein = $("select#top1").val();
+    var spice = $("select#top2").val();
+    var veggie = $("select#top3").val();
+    var newPizza = new Pizza (size, protein, spice, veggie);
+    Pizza.addTops(newPizza);
 
 
 
