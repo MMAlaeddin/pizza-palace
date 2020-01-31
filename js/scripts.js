8ce0e94fed719776f2eca571dfb734635bca3112
Student <example@example.com>
2020-01-31 10:28:42 -0800

    remove values, add if else

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,20 @@ function Pizza (size, top1, top2, top3) {
 
 }
 
+function Tops () {
+  this.pizza = []
+}
+
+Pizza.prototype.addPizza = function(){
+  Pizza.push(myPizza)
+  
+
+
+}
+
+
+
+
 
 
 
@@ -25,7 +39,14 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
 
-    
+    var size = $("select#size").val();
+    var top1 = $("select#top1").val();
+    var top2 = $("select#top2").val();
+    var top3 = $("select#top3").val();
+
+    var myPizza = new Pizza (size, top1, top2, top3)
+
+    $(".price").text(myPizza);
 
 
   })
